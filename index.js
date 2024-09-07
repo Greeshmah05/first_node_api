8e0ade80687c0e5688ba8626e17cd5fc8e7205c2
Greeshmah05 <greeshmahbhandary2005@gmail.com>
2024-09-07 19:50:18 +0530

    Initial commit with my index.js file

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("My first node js program");
+const http = require('http');
+ 
+ // Create an HTTP server
+ const server = http.createServer((req, res) => {
+     // Set the response HTTP header with HTTP status and Content-Type
+     res.statusCode = 200;
+     res.setHeader('Content-Type', 'text/plain');
+ 
+     // Send the response body
+     res.end('<h1>Hello, World!</h1>\n');
+ });
+ 
+ // Define the port to listen to
+ const port = 3000;
+ 
+ // The server listens on port 3000
+ server.listen(port, () => {
+     console.log(`Server running at http://localhost:${port}/`);
+ });
